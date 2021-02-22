b15900a5b26fc5a9aaa53568f44d233e59b239e6
derrek-gass <dardcore@gmail.com>
2021-02-21 20:20:37 -0800

    mergesort complete

--- src/SortingVisualizer/SortingVisualizer.jsx
+++ src/SortingVisualizer/SortingVisualizer.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
 import './SortingVisualizer.css';
+import {getMergeSortAnimations} from '../SortingAlgorithms/sortingAlgorithms.js';
+
+const ANIMATION_SPEED_MS =1;
+
+const NUMBER_OF_ARRAY_BARS = 310;
+
+const PRIMARY_COLOR = 'turquoise';
+
+const SECONDARY_COLOR = 'red';
+
 
 export default class SortingVisualizer extends React.Component {
     constructor(props){
         super(props);
+
         this.state = {
             array: [],
 
@@ -16,27 +27,91 @@ export default class SortingVisualizer extends React.Component {
 
     resetArray(){
         const array = [];
-        for (let i=0; i<100; i++){
-            array.push(randomIntFromInterval(5, 1000));
+        for (let i = 0; i < NUMBER_OF_ARRAY_BARS; i++){
+            array.push(randomIntFromInterval(5, 730));
         }
         this.setState({array});
     }
+
+    mergeSort() {
+    const animations = getMergeSortAnimations(this.state.array);
+    for (let i = 0; i < animations.length; i++) {
+      const arrayBars = document.getElementsByClassName('array-bar');
+      const isColorChange = i % 3 !== 2;
+      if (isColorChange) {
+        const [barOneIdx, barTwoIdx] = animations[i];
+        const barOneStyle = arrayBars[barOneIdx].style;
+        const barTwoStyle = arrayBars[barTwoIdx].style;
+        const color = i % 3 === 0 ? SECONDARY_COLOR : PRIMARY_COLOR;
+        setTimeout(() => {
+          barOneStyle.backgroundColor = color;
+          barTwoStyle.backgroundColor = color;
+        }, i * ANIMATION_SPEED_MS);
+      } else {
+        setTimeout(() => {
+          const [barOneIdx, newHeight] = animations[i];
+          const barOneStyle = arrayBars[barOneIdx].style;
+          barOneStyle.height = `${newHeight}px`;
+        }, i * ANIMATION_SPEED_MS);
+      }
+    }
+    }
+
+    quickSort() {}
+
+    heapSort() {}
+
+    bubbleSort() {}
+
+    testSortingAlgorithms(){
+        for (let i=0; i< 100; i++){
+            const array = [];
+            const length = randomIntFromInterval(1, 1000);
+            for (let i=0; i< length; i++){
+                array.push(randomIntFromInterval(-1000, 1000));
+            }
+            const javaScriptSortedArray = array
+                .slice()
+                .sort((a, b) => a-b);
+            const mergeSortedArray = getMergeSortAnimations(array.slice());       
+            console.log(arraysAreEqual(javaScriptSortedArray, mergeSortedArray));            
+        }
+    }
+
+
+
 
     render(){
         const {array} = this.state;
 
         return (
-            <>
+            <div className="array-container">
                 {array.map((value, index) =>(
-                    <div classname="array-bar" key={index}>
-                        {value}
-                    </div>
+                    <div 
+                    className="array-bar" 
+                    key={index}
+                    style={{height: `${value}px`}}></div>
                 ))}
-            </>
+                <br/>
+                <button onClick={()=> this.resetArray()}>Reset Array</button>
+                <button onClick={()=> this.mergeSort()}>Merge Sort</button>
+                <button onClick={()=> this.quickSort()}>Quick Sort</button>
+                <button onClick={()=> this.heapSort()}>Heap Sort</button>
+                <button onClick={()=> this.bubbleSort()}>Bubble Sort</button>
+                <button onClick={()=> this.testSortingAlgorithms()}>Test Sorting Algorithms (BROKEN)</button>
+            </div>
         );
     }
 }
 
 function randomIntFromInterval(min, max){
     return Math.floor(Math.random() * (max - min + 1) + min);    
+}
+
+function arraysAreEqual(arrOne, arrTwo){
+    if (arrOne.length !== arrTwo.length) return false;
+    for (let i=0; i< arrOne.length; i++){
+        if (arrOne[i] !== arrTwo[i]) return false;
+    }
+    return true;
 }
