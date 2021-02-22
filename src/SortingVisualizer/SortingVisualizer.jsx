fa793373c817ca8d4e9313b56fdc5d4267b89f59
derrek-gass <dardcore@gmail.com>
2021-02-21 18:26:23 -0800

    randomint function

--- src/SortingVisualizer/SortingVisualizer.jsx
+++ src/SortingVisualizer/SortingVisualizer.jsx
@@ -36,3 +36,7 @@ export default class SortingVisualizer extends React.Component {
         );
     }
 }
+
+function randomIntFromInterval(min, max){
+    return Math.floor(Math.random() * (max - min + 1) + min);    
+}
